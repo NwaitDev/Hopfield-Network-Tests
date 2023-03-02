b2844942fccdbf1a5562dd75022b46e52c5a40f3
NwaitDev <nathangallone@hotmail.com>
2023-03-02 12:04:52 +0100

    Describing + precisely this projects requirements

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Hopfield-Network-Tests
 Programming some hopfield networks for educational purposes
+
+Python 3.10.9 compatible
+requires matplotlib, numpy, PIL
--- hopfield-test.py
+++ hopfield-test.py
@@ -1,4 +1,13 @@
 import numpy as np
+import src.imageToMatrix as itm
 
-"""Type your code here"""
+"""
+We need :
+	- a matrix that represents the network (if retreiving N*N images : it should be a (N²,N²) matrix)
+	such matrix is related to the mean of the images stored in the matrix
+	- a functions that takes an image (/a key) and applies it the matrix calculus 
+	to produce the next step to the expected result (it can have two modes : synchronous and asynchonous)
+	- a function that iterates over the previous one to compute, step by step the target final image
+	- an interface function that allows to use the functionnalities of this API
+"""
 
