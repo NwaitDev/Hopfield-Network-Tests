47549e5abb24dbf3383f55d3538535596c5cf2d5
NwaitDev <nathangallone@hotmail.com>
2023-03-02 17:44:04 +0100

    re adding first test

--- validate.py
+++ validate.py
@@ -4,10 +4,18 @@ import hopfieldnet as hn
 import src.imageToMatrix as itm
 
 
-if __name__ == "__main__":
+def basicTestSmallImage():
 	img = [[0,1],[0,1]]
 	expected = np.array([[0,0,0,0],[0,0,0,1],[0,0,0,0],[0,1,0,0]])
 	actual = hn.networkFromImages([img],imgHeight=2,imgWidth=2)
 	
 	test("test testing",actual,expected)
 	printThatMatrix(img)
+
+if __name__ == "__main__":
+	path = "img-data/eye64x64.jpg"
+	img = itm.importToMatrix(path)
+	network = hn.networkFromImages([img],imgHeight=64,imgWidth=64)
+
+	printThatMatrix(img)
+	printThatMatrix(network)
