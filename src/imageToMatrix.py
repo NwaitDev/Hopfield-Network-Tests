0c30b811ddb1d8b86af0db1f5504af45615be146
NwaitDev <nathangallone@hotmail.com>
2023-03-02 11:28:35 +0100

    First commit from me

---
+++ src/imageToMatrix.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+print(os.getcwd())  
+
+img = Image.open(r"img-data/smile64x64.jpg")
+arr = np.array(img)
+
+def rgbToGreyScale(rgbArray):
+	return (int(rgbArray[0])+int(rgbArray[1])+int(rgbArray[2]))/(3.0*255)
+
+def rgbMatrixToGreyScale(rgbMatrix):
+	arr = []
+	for i in range(len(rgbMatrix)):
+		for j in range(len(rgbMatrix[i])):
+			arr.append(rgbToGreyScale(rgbMatrix[i][j]))
+	return np.resize(np.array(arr),(64,64))
+
+arr = rgbMatrixToGreyScale(arr)
+
+imgplot = plt.imshow(arr)
