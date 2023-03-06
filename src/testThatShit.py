c42554929129f070fde1188955d24ddb92cd9e97
Jocelyn-Bournier <bournierjocelyn@yahoo.fr>
2023-03-06 13:42:45 +0100

    testJo

--- src/testThatShit.py
+++ src/testThatShit.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from colorama import Fore, Style
 from matplotlib.colors import ListedColormap
 
+
 def failMessage(testName, actual, expected):
 	print(Fore.RED + "[✗] : %s\nactual :\n%s\nexpected :\n%s"%(testName,str(actual),str(expected))+Style.RESET_ALL)
 
